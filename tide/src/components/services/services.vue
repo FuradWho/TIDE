490f11702401cd8709c50f0fb7291df8f07f9dc4
FuradWho <liu1337543811@gmail.com>
2022-10-27 22:48:15 +0800

    modify pages

--- tide/src/components/services/services.vue
+++ tide/src/components/services/services.vue
@@ -1,209 +1,308 @@
 <template>
   <div class="P-about">
     <div style="position: relative">
-      <img src="../../assets/SERVICES/线条插图-1.png" style="width: 100%; position: absolute" />
-      <div style="
+      <img
+        src="../../assets/SERVICES/线条插图-1.png"
+        style="width: 100%; position: absolute"
+      />
+      <div
+        style="
           font-size: 20px;
           font-weight: bold;
           position: absolute;
           top: 40px;
           left: 995px;
-        ">
+        "
+      >
         <span>SERVICES</span>
       </div>
 
-      <div style="
+      <div
+        style="
           font-size: 20px;
           font-weight: bold;
           position: absolute;
           top: 58px;
           left: 1030px;
-        ">
+        "
+      >
         <span>——</span>
       </div>
 
-      <div style="
+      <div
+        style="
           font-size: 48px;
           font-weight: bold;
           position: absolute;
           top: 148px;
           left: 750px;
-        ">
+        "
+      >
         <span>Mooning with Tide Capital</span>
       </div>
 
-      <div>
-        <img src="../../assets/SERVICES/Liquidity Provivion/Liquidity Provivion-3.png" style="
-            position: absolute;
-            width: 108px;
-            top: 450px;
-            left: 230px;
-          " />
-        <div style="
+      <div class="e-div-service-1">
+        <img
+          class="e-img-service-1"
+          src="../../assets/SERVICES/LP/LP-1.png"
+          style="position: absolute; width: 108px; top: 450px; left: 230px"
+        />
+        <div
+          style="
             position: absolute;
             width: 570px;
             height: 350px;
             top: 400px;
             left: 380px;
             border-radius: 20px;
-            background-color: rgba(233, 241, 248,1);
-          ">
-          <span style="position: absolute;font-size: 32px;font-weight: 700;top: 60px;left: 35px;">Liquidity
-            Provision</span>
-          <span style="position: absolute;font-size: 20px;top: 140px;left: 35px;">We have helped 100+ tokens with their
-            liquidity, </span>
-          <span style="position: absolute;font-size: 20px;top: 180px;left: 35px;">facilitated smoothing the trading
-            experience, enhance </span>
-          <span style="position: absolute;font-size: 20px;top: 220px;left: 35px;">the liquidity depth, reduce the
-            spread, and generate </span>
-          <span style="position: absolute;font-size: 20px;top: 260px;left: 35px;">natural trading volume at a minimum
-            cost. </span>
+            background-color: rgba(233, 241, 248, 1);
+          "
+        >
+          <span
+            style="
+              position: absolute;
+              font-size: 32px;
+              font-weight: 700;
+              top: 60px;
+              left: 35px;
+            "
+            >Liquidity Provision</span
+          >
+          <span
+            style="position: absolute; font-size: 20px; top: 140px; left: 35px"
+            >We have helped 100+ tokens with their liquidity,
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 180px; left: 35px"
+            >facilitated smoothing the trading experience, enhance
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 220px; left: 35px"
+            >the liquidity depth, reduce the spread, and generate
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 260px; left: 35px"
+            >natural trading volume at a minimum cost.
+          </span>
         </div>
       </div>
 
-      <div>
-        <img src="../../assets/SERVICES/Financial Advisory/Financial Advisory-3.png" style="
-            position: absolute;
-            width: 108px;
-            top: 880px;
-            left: 230px;
-          " />
-        <div style="
+      <div class="e-div-service-2">
+        <img
+          class="e-img-service-2"
+          src="../../assets/SERVICES/FA/FA-1.png"
+          style="position: absolute; width: 108px; top: 880px; left: 230px"
+        />
+        <div
+          style="
             position: absolute;
             width: 570px;
             height: 350px;
             top: 830px;
             left: 380px;
             border-radius: 20px;
-            background-color: rgba(233, 241, 248,1);
-          ">
-          <span style="position: absolute;font-size: 32px;font-weight: 700;top: 60px;left: 35px;">Financial
-            Advisory</span>
-          <span style="position: absolute;font-size: 20px;top: 140px;left: 35px;">We have strong connections with some
-            of the top VCs </span>
-          <span style="position: absolute;font-size: 20px;top: 180px;left: 35px;">in the industry. We make joint
-            investments with our </span>
-          <span style="position: absolute;font-size: 20px;top: 220px;left: 35px;">partners and provide some exclusive
-            investment </span>
-          <span style="position: absolute;font-size: 20px;top: 260px;left: 35px;">opportunities and financial
-            advisorship for projects.</span>
+            background-color: rgba(233, 241, 248, 1);
+          "
+        >
+          <span
+            style="
+              position: absolute;
+              font-size: 32px;
+              font-weight: 700;
+              top: 60px;
+              left: 35px;
+            "
+            >Financial Advisory</span
+          >
+          <span
+            style="position: absolute; font-size: 20px; top: 140px; left: 35px"
+            >We have strong connections with some of the top VCs
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 180px; left: 35px"
+            >in the industry. We make joint investments with our
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 220px; left: 35px"
+            >partners and provide some exclusive investment
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 260px; left: 35px"
+            >opportunities and financial advisorship for projects.</span
+          >
         </div>
       </div>
 
-      <div>
-        <img src="../../assets/SERVICES/Market Services/Market Services-3.png" style="
-            position: absolute;
-            width: 108px;
-            top: 1310px;
-            left: 230px;
-          " />
-        <div style="
+      <div class="e-div-service-3">
+        <img
+          class="e-img-service-3"
+          src="../../assets/SERVICES/MS/MS-1.png"
+          style="position: absolute; width: 108px; top: 1310px; left: 230px"
+        />
+        <div
+          style="
             position: absolute;
             width: 570px;
             height: 350px;
             top: 1270px;
             left: 380px;
             border-radius: 20px;
-            background-color: rgba(233, 241, 248,1);
-          ">
-          <span style="position: absolute;font-size: 32px;font-weight: 700;top: 60px;left: 35px;">Marketing
-            Services</span>
-          <span style="position: absolute;font-size: 20px;top: 140px;left: 35px;">In-depth understanding of Web3.
-            industry enables us </span>
-          <span style="position: absolute;font-size: 20px;top: 180px;left: 35px;">to create an outstanding narrative;
-            Connections with </span>
-          <span style="position: absolute;font-size: 20px;top: 220px;left: 35px;">most media platforms make sure that
-            your voice can </span>
-          <span style="position: absolute;font-size: 20px;top: 260px;left: 35px;">be heard by mass audience. </span>
+            background-color: rgba(233, 241, 248, 1);
+          "
+        >
+          <span
+            style="
+              position: absolute;
+              font-size: 32px;
+              font-weight: 700;
+              top: 60px;
+              left: 35px;
+            "
+            >Marketing Services</span
+          >
+          <span
+            style="position: absolute; font-size: 20px; top: 140px; left: 35px"
+            >In-depth understanding of Web3. industry enables us
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 180px; left: 35px"
+            >to create an outstanding narrative; Connections with
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 220px; left: 35px"
+            >most media platforms make sure that your voice can
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 260px; left: 35px"
+            >be heard by mass audience.
+          </span>
         </div>
       </div>
 
-      <div>
-        <img src="../../assets/SERVICES/Value management/Value management-3.png" style="
-            position: absolute;
-            width: 108px;
-            top: 450px;
-            left: 1100px;
-          " />
-        <div style="
+      <div class="e-div-service-4">
+        <img
+          class="e-img-service-4"
+          src="../../assets/SERVICES/VM/VM-1.png"
+          style="position: absolute; width: 108px; top: 450px; left: 1100px"
+        />
+        <div
+          style="
             position: absolute;
             width: 570px;
             height: 350px;
             top: 400px;
             left: 1250px;
             border-radius: 20px;
-            background-color: rgba(233, 241, 248,1);
-          ">
-          <span style="position: absolute;font-size: 32px;font-weight: 700;top: 60px;left: 35px;">Value
-            Management</span>
-          <span style="position: absolute;font-size: 20px;top: 140px;left: 35px;">ith multiple high-performance
-            strategies, are able to </span>
-          <span style="position: absolute;font-size: 20px;top: 180px;left: 35px;">rebuild a natural upward going
-            channel. A suite of </span>
-          <span style="position: absolute;font-size: 20px;top: 220px;left: 35px;">tools can be used for the bottom
-            building and buy </span>
-          <span style="position: absolute;font-size: 20px;top: 260px;left: 35px;">back, automate the token liquidation.
+            background-color: rgba(233, 241, 248, 1);
+          "
+        >
+          <span
+            style="
+              position: absolute;
+              font-size: 32px;
+              font-weight: 700;
+              top: 60px;
+              left: 35px;
+            "
+            >Value Management</span
+          >
+          <span
+            style="position: absolute; font-size: 20px; top: 140px; left: 35px"
+            >ith multiple high-performance strategies, are able to
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 180px; left: 35px"
+            >rebuild a natural upward going channel. A suite of
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 220px; left: 35px"
+            >tools can be used for the bottom building and buy
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 260px; left: 35px"
+            >back, automate the token liquidation.
           </span>
         </div>
       </div>
 
-      <div>
-        <img src="../../assets/SERVICES/Listing/Listing-3.png" style="
-            position: absolute;
-            width: 108px;
-            top: 880px;
-            left: 1100px;
-          " />
-        <div style="
+      <div class="e-div-service-5">
+        <img
+          class="e-img-service-5"
+          src="../../assets/SERVICES/L/L-1.png"
+          style="position: absolute; width: 108px; top: 880px; left: 1100px"
+        />
+        <div
+          class="e-div-service-5"
+          style="
             position: absolute;
             width: 570px;
             height: 350px;
             top: 830px;
             left: 1250px;
             border-radius: 20px;
-            background-color: rgba(233, 241, 248,1);
-          ">
-          <span style="position: absolute;font-size: 32px;font-weight: 700;top: 60px;left: 35px;">Listing</span>
-          <span style="position: absolute;font-size: 20px;top: 140px;left: 35px;">In-depth research on exchange
-            liquidity; Precise </span>
-          <span style="position: absolute;font-size: 20px;top: 180px;left: 35px;">estimation on first day liquidation;
-            Well-rounded </span>
-          <span style="position: absolute;font-size: 20px;top: 220px;left: 35px;">advisory on choosing exchanges; Solid
-            connections </span>
-          <span style="position: absolute;font-size: 20px;top: 260px;left: 35px;">with all tier1 and tier2 exchanges.
+            background-color: rgba(233, 241, 248, 1);
+          "
+        >
+          <span
+            style="
+              position: absolute;
+              font-size: 32px;
+              font-weight: 700;
+              top: 60px;
+              left: 35px;
+            "
+            >Listing</span
+          >
+          <span
+            style="position: absolute; font-size: 20px; top: 140px; left: 35px"
+            >In-depth research on exchange liquidity; Precise
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 180px; left: 35px"
+            >estimation on first day liquidation; Well-rounded
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 220px; left: 35px"
+            >advisory on choosing exchanges; Solid connections
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 260px; left: 35px"
+            >with all tier1 and tier2 exchanges.
           </span>
         </div>
       </div>
 
-      <div class="da">
-        <img class="oa" src="../../assets/SERVICES/Product and OA/Product and OA.png" style="
-            position: absolute;
-            width: 108px;
-            top: 1330px;
-            left: 1100px;
-          " />
-        <div style="
-            position: absolute;
-            width: 570px;
-            height: 350px;
-            top: 1270px;
-            left: 1250px;
-            border-radius: 20px;
-            background-color: rgba(233, 241, 248,1);
-          ">
-          <span style="position: absolute;font-size: 32px;font-weight: 700;top: 60px;left: 35px;">Product and Operating
-            Advisory</span>
-          <span style="position: absolute;font-size: 20px;top: 140px;left: 35px;">Our advisory team has years of
-            experience in project </span>
-          <span style="position: absolute;font-size: 20px;top: 180px;left: 35px;">operation and product designs. We are
-            familiar with </span>
-          <span style="position: absolute;font-size: 20px;top: 220px;left: 35px;">web3 audience and are making it an
-            easy task for </span>
-          <span style="position: absolute;font-size: 20px;top: 260px;left: 35px;">enterprises trying to enter this
-            field. </span>
+      <div class="e-div-service-6">
+        <img class="e-img-service-6" src="../../assets/SERVICES/PO/PO-1.png" />
+        <div class="e-div1-service-6">
+          <span
+            style="
+              position: absolute;
+              font-size: 32px;
+              font-weight: 700;
+              top: 60px;
+              left: 35px;
+            "
+            >Product and Operating Advisory</span
+          >
+          <span
+            style="position: absolute; font-size: 20px; top: 140px; left: 35px"
+            >Our advisory team has years of experience in project
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 180px; left: 35px"
+            >operation and product designs. We are familiar with
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 220px; left: 35px"
+            >web3 audience and are making it an easy task for
+          </span>
+          <span
+            style="position: absolute; font-size: 20px; top: 260px; left: 35px"
+            >enterprises trying to enter this field.
+          </span>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -217,12 +316,54 @@ export default {};
   height: 1700px;
 }
 
-.da:hover .oa {
-  content: url("../../assets/SERVICES/Product and OA/Product and OA-3.png");
-  transform: scale(1.06); 
+.e-div-service-1:hover .e-img-service-1 {
+  content: url("../../assets/SERVICES/LP/LP-3.png");
+  transform: scale(1.06);
   transition: all 1s;
-    /* opacity:0;
-    filter: alpha(opacity=0); */
+}
+
+.e-div-service-2:hover .e-img-service-2 {
+  content: url("../../assets/SERVICES/FA/FA-3.png");
+  transform: scale(1.06);
+  transition: all 1s;
+}
+
+.e-div-service-3:hover .e-img-service-3 {
+  content: url("../../assets/SERVICES/MS/MS-3.png");
+  transform: scale(1.06);
+  transition: all 1s;
+}
+.e-div-service-4:hover .e-img-service-4 {
+  content: url("../../assets/SERVICES/VM/VM-3.png");
+  transform: scale(1.06);
+  transition: all 1s;
+}
+.e-div-service-5:hover .e-img-service-5 {
+  content: url("../../assets/SERVICES/L/L-3.png");
+  transform: scale(1.06);
+  transition: all 1s;
+}
+
+.e-img-service-6 {
+  position: absolute;
+  width: 108px;
+  top: 1330px;
+  left: 1100px;
+}
+.e-div-service-6:hover .e-img-service-6 {
+  content: url("../../assets/SERVICES/PO/PO-3.png");
+  transform: scale(1.06);
+  transition: all 1s;
+}
+
+.e-div1-service-6 {
+  position: absolute;
+  width: 570px;
+  height: 350px;
+  top: 1270px;
+  left: 1250px;
+  border-radius: 20px;
+  background-color: rgba(233, 241, 248, 1);
 }
 </style>
   
